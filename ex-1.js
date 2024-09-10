576950e827daf21dc17c4ad15d4038fbcfbb69cd
FrankCW <wongasa999@gmail.com>
2024-09-10 11:14:41 +0700

    Boolean-and-Logic Ex1-3

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,13 @@
-let isOver18;
-let hasCriminalBlacklist;
+let isOver18; 
+let hasCriminalBlacklist; 
 
 let isAllow;
 
 // Start coding here
 
 console.log(isAllow);
+
+isOver18 = true;
+hasCriminalBlacklist = true;
+isAllow = isOver18 && hasCriminalBlacklist;
+console.log(isAllow)
